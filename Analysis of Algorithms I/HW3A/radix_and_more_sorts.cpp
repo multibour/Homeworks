dc699d985d844b45738f5482c427750fc7e58b95
Kaan Atukalp <katukalp@gmail.com>
2017-01-30 12:43:20 +0300

    Create radix_and_more_sorts.cpp

---
+++ Analysis of Algorithms I/HW3A/radix_and_more_sorts.cpp
@@ -0,0 +1,36 @@
+#include <cstdlib>
+#include <ctime>
+#include "sorts_P3_1.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+	if (argc != 2){
+		cout << "Invalid number of inputs." << endl;
+		return EXIT_FAILURE;
+	}
+
+	Sorting sorting(atoi(argv[1]));
+	sorting.readFile("numbers_to_sort.txt");
+
+	cout << "Sorting..." << endl;
+	clock_t elapsedTime = clock();
+	sorting.radixSort();
+	elapsedTime -= clock();
+	cout << "Elapsed time: " << elapsedTime << " seconds" << endl;
+
+	sorting.writeFile("sorted_numbers.txt");
+
+	/*{
+		long long int* arr1 = sorting.getCopy();
+		long long int* arr2 = sorting.getCopy();
+
+		gnomeSort(arr1, sorting.getSize());
+		freezingSort(arr2, sorting.getSize());
+	
+		delete[] arr1;
+		delete[] arr2;
+	}*/
+
+	return EXIT_SUCCESS;
+}
