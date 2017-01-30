42179a8bed4cdbb5e91d6d0c07390cc2bce46f3e
Kaan Atukalp <katukalp@gmail.com>
2017-01-30 12:50:36 +0300

    Create heap_sort.cpp

---
+++ Analysis of Algorithms I/HW3B/heap_sort.cpp
@@ -0,0 +1,33 @@
+#include "Header_P3_2.h"
+#include <ctime>
+
+using namespace std;
+
+
+int main(int argc, char* argv[]){
+	if (argc == 2){// ./p 2
+		Game game;
+
+		game.play();
+	}
+	else if (argc == 3){// ./p 1 N
+		Players clanA(atoi(argv[2]));
+
+		clanA.readFile("ClanA.txt");
+
+		cout << "Heap Sort..." << endl;
+		clock_t elapsedTime = clock();
+		clanA.heapSort();
+		elapsedTime -= clock();
+		cout << "Elapsed time: " << elapsedTime << " seconds" << endl;
+
+		clanA.writeFile("A_sorted.txt");
+	}
+	else{
+		cout << "Invalid number of inputs." << endl;
+
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
